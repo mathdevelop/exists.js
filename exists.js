a103dc4cf39c9bb0e5086080ef52390490039725
Matheus Alves <web.matheusalves@gmail.com>
2020-06-28 19:23:42 -0300

    Transforming into function

--- exists.js
+++ exists.js
@@ -1,17 +1,14 @@
-Object.defineProperty(Object.prototype, 'exists', {
-    value: function(selector, parentElement = null) {
-        return new Promise(function(resolve) {
-			var observer = new MutationObserver(function() {
-				if(document.querySelector(selector)) {
-					observer.disconnect();
+function exists(selector, parentElement = null) {
+	return new Promise(function(resolve) {
+		var observer = new MutationObserver(function() {
+			if(document.querySelector(selector)) {
+				observer.disconnect();
 
-					resolve();
-				} 
-			}),
-			target = parentElement ? document.querySelector(parentElement) ?? document : document;
+				resolve();
+			} 
+		}),
+		target = parentElement ? document.querySelector(parentElement) ?? document : document;
 
-			observer.observe(target, { childList: true, subtree: true });
-		});
-    },
-    enumerable: false
-});
+		observer.observe(target, { childList: true, subtree: true });
+	});
+}
